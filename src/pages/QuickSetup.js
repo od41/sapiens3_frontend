3f315e24bc6f157357476f144049da170ff2a556
Odafe <odafe41@gmail.com>
2022-05-08 14:22:15 +0000

    first pass @ quick setup page

--- src/pages/QuickSetup.js
+++ src/pages/QuickSetup.js
@@ -5,58 +5,65 @@ import { Link, Outlet } from 'react-router-dom';
 import quickSetup from 'assets/img/scenery/quick_setup.jpg'
 
 function QuickSetup() {
+
+  const heading2 = {
+    fontWeight: '600',
+    fontSize: '32px',
+    color: '#0a0a0a'
+  }
+
   return (
     <>
-      <section className="clean-block clean-info dark" style={{background: "var(--bs-white)"}}>
-            <div className="container" style={{margintop: "45px"}}>
-                <div className="row align-items-center">
-                    <div className="col-md-6">
-                        <h3><strong>Find a roomie.</strong><br /><strong>with Shared values.</strong></h3>
-                        <div className="getting-started-info">
-                            <form>
-                              <div>
-                                <label for="first_name">First Name</label>
-                                <input name="first_name" id='first_name' type='text' />
-                              </div>
-
-                              <div>
-                                <label for="last_name">Last Name</label>
-                                <input name="last_name" id='last_name' type='text' />
-                              </div>
-
-                              <div>
-                                <label for="date_of_birth">Date of birth</label>
-                                <input name="date_of_birth" id='date_of_birth' type='date' />
-                              </div>
-
-                              <div>
-                                <label for="gender">Gender</label>
-                                <span>
-                                  <input name="gender" id='male' value="male" type='radio' />
-                                  <label for="male">Male</label>
-                                </span>
-                                <span>
-                                  <input name="gender" id='female' value="female" type='radio' />
-                                  <label for="female">Female</label>
-                                </span>
-                              </div>
-
-                              <div>
-                                <label for="desired_location">Desired location</label>
-                                <input name="desired_location" id='desired_location' type='text' />
-                              </div>
-                              <button className="btn btn-outline-primary btn-lg" type="button" style={{background: "var(--bs-gray-dark)", color: "var(--bs-white)", borderRadius: "15px", textAlign: "center"}}>Find a roomie</button>
-
-                            </form>
-                        </div>
-                        <div className="getting-started-info">
-                            <img alt="quick setup" src={quickSetup} />
-                        </div>
+     <header className="masthead">
+        <div className="container">
+            <div className="row align-items-center">
+                <div className="col-md-6" style={{borderRadius: "20"}}>
+                    <h3 style={ {...heading2, marginTop: "123px"}} >Find A Roomie</h3>
+                    <div className="getting-started-info">
+                        <p>Fill this form and we will get you started right away.</p>
                     </div>
-                    <div className="col-md-6"></div>
+                    <form>
+                        <div className="row">
+                            <div className="col-sm-6">
+                                <div className="form-group mb-3"><label className="form-label">Date of Birth</label>
+                                  <input className="form-control" type="text" style={{borderRadius: "20px"}} />
+                                </div>
+                            </div>
+                            <div className="col-sm-6">
+                                <div className="form-group mb-3"><label className="form-label">Display Image</label>
+                                  <input className="form-control" type="text" style={{borderRadius: "16px"}} />
+                                </div>
+                            </div>
+                        </div>
+                        <div className="row" style={{textAlign: "left"}}>
+                            <div className="col-sm-1 col-lg-11 col-xl-10" style={{width: "auto"}}>
+                                <div className="form-group mb-3"><label className="form-label">Gender</label></div>
+                                <div className="form-group mb-3" style={{textAlign: "center", width: "556px"}}>
+                                    <div className="btn-group" role="group" style={{textAlign: "center", borderRadius: 0}}>
+                                      <button className="btn btn-primary" type="button" style={{"color: var(--bs-gray-900);background: var(--bs-white);border-radius: 18px;margin-right: 22px;border-width: 3px;border-color: var(--bs-gray);padding-right: 36px;padding-left: 36px;width: 219.5px;": ""}}>Male</button>
+                                      <button className="btn btn-primary" type="button" style={{"border-radius: 20px;color: var(--bs-gray-800);background: var(--bs-white);max-width: 218px;border-width: 3px;border-color: var(--bs-gray-700);width: 219.5px;": ""}}>FEMALE</button>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                        <div className="row">
+                            <div className="col-sm-6 col-lg-12">
+                                <div className="form-group mb-3"><label className="form-label">Desired Location</label>
+                                  <input className="form-control" type="text" style={{borderRadius: "20px"}} />
+                                </div>
+                            </div>
+                        </div>
+                    </form>
+                    <div className="getting-started-info"><button className="btn btn-outline-primary btn-lg" type="button" style={{"background: var(--bs-gray-dark);color: var(--bs-white);border-radius: 15px;text-align: center;padding-right: 25px;margin-top: 11px;":""}}>Find a roomie</button>
+                        <p></p>
+                    </div>
+                </div>
+                <div className="col-md-6">
+                  <img className="img-thumbnail" alt="promo banner" data-aos="fade-up" data-aos-duration="100" src={quickSetup} />
                 </div>
             </div>
-        </section>
+        </div>
+    </header>
     
     </>
   )
